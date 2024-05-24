2e6713ef0e494f1c0782ab1ef054223d8f09cb2b
Vladimir Logachev <vladimir@logachev.dev>
2024-05-24 05:31:26 +0400

    Parse records from file in streams

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,36 +12,61 @@ import AssetAmount._
 import AssetPrice._
 
 object Main extends ZIOAppDefault {
-  private def main: IO[Throwable, Unit] = {
-    val clientsStream = ZStream
-      .fromFileName("clients.txt")
+  def balancesFromFile(fileName: String) =
+    ZStream
+      .fromFileName(fileName)
       .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
+      .mapZIO(s =>
+        ClientBalanceRecord.syntax.parseString(s) match {
+          case Left(_)  => ZIO.fail(new IOException("Failed to parse client balance"))
+          case Right(v) => ZIO.succeed(v)
+        }
+      )
 
-    val ordersStream = ZStream
-      .fromFileName("orders.txt")
-      .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
-    val output = runMatcher(clients, clientOrders)
-
-    def fileSink(path: Path): ZSink[Any, Throwable, String, Byte, Long] =
+  def balancesToFile(fileName: String, balances: List[ClientBalanceRecord]) =
+    val fileSink =
       ZSink
-        .fromPath(path)
+        .fromPath(Paths.get(fileName))
         .contramapChunks[String](_.flatMap(_.getBytes))
 
-    val result = ZStream("Hello", "ZIO", "World!")
+    ZStream
+      .fromIterable(balances)
+      .mapZIO(v =>
+        ClientBalanceRecord.syntax.printString(v) match {
+          case Left(_)  => ZIO.fail(new IOException("Failed to parse client balance"))
+          case Right(s) => ZIO.succeed(s)
+        }
+      )
       .intersperse("\n")
-      .run(fileSink(Paths.get("results.txt")))
+      .run(fileSink)
+
+  def ordersFromFile(fileName: String) =
+    ZStream
+      .fromFileName(fileName)
+      .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
+      .mapZIO(s =>
+        clientOrderSyntax.parseString(s) match {
+          case Left(_)  => ZIO.fail(new IOException("Failed to parse client order"))
+          case Right(v) => ZIO.succeed(v)
+        }
+      )
+
+  private def main: IO[Throwable, Unit] = {
+
+    val output = runMatcher(clients, clientOrders)
 
     for {
-      _ <- clientsStream.foreach(printLine(_))
-      _ <- ordersStream.foreach(printLine(_))
+      _ <- balancesFromFile("clients.txt").foreach(printLine(_))
+      _ <- ordersFromFile("orders.txt").foreach(printLine(_))
+
       _ <- printLine(output)
 
       // TODO: write actual outputs to file
-      _ <- ZStream
-        .fromFileName("clients.txt")
-        .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
-        .intersperse("\n")
-        .run(fileSink(Paths.get("results.txt")))
+      stubOutputBalances <- balancesFromFile("clients.txt").run(
+        ZSink.foldLeft(List.empty[ClientBalanceRecord])((b, a) => b ++ List(a))
+      )
+      _ <- balancesToFile("results.txt", stubOutputBalances)
+
     } yield ()
   }
 
