a3502a12da7741c76bd2556e7dd1611455e463eb
Scala Steward <scala_steward@virtuslab.com>
2025-03-01 23:47:04 +0000

    Reformat with scalafmt 3.8.6
    Executed command: scalafmt --non-interactive

--- src/main/scala/exchange/domain/model/OrderBook.scala
+++ src/main/scala/exchange/domain/model/OrderBook.scala
@@ -19,7 +19,6 @@ import exchange.domain.model.AssetPrices._
   * Note on scalaz's Dequeue naming:
   *   - snoc(a: A): Dequeue[A] – enqueue on to the back of the queue (similar to `enqueue` in Queue)
   *   - uncons: Maybe[(A, Dequeue[A])] – dequeue from the front of the queue (similar to `dequeueOption` in Queue)
-  *
   *   - cons(a: A): Dequeue[A] – enqueue to the front of the queue (when putting partially filled order back) (unique to
   *     Dequeue)
   */
